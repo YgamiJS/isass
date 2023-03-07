4902cb311f92d7e850facf949aab494468ac0020
Dima <dimasakharov@icloud.com>
2023-03-07 19:31:08 +0300

    да

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-slate-600">
+      hjh
+      <div className="btn">
+        <div className="o">tet</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
